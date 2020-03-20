2d428355e4df7ebd9c9123ec05b30cb24ae664fe
Julia <qq.julia23@gmail.com>
2020-03-20 21:33:42 +0000

    v2

--- index.rb
+++ index.rb
@@ -1,60 +1,65 @@
 class Station
-  attr_accessor :passenger_train
+  attr_reader :name
 
-  def initialize(station_name)
-    @station_name = station_name
-    @cargo_train = []
-    @passenger_train = []
+  def initialize(name)
+    @name = name
+    @trains ={}
   end
 
   def add_train(train)
-    if train.type == "passenger"
-      @passenger_train.push(train.num)
-    elsif train.type == "cargo"
-      @cargo_train.push(train.num)
+    unless @trains.include?train.type
+      @trains[train.type] = []
     end
+      @trains[train.type].push(train)
+      puts "Прибыл поезд #{train.type} #{train.num}"
   end
 
-  def delete_train(train)
-    if train.type == "passenger"
-      @passenger_train.delete(train.num)
-    elsif train.type == "cargo"
-      @cargo_train.delete(train.num)
-    end
-    puts "Со станции отбыл поезд: #{train.num}"
-  end
+   def delete_train(train)
+     @trains[train.type].delete(train)
+   end
+
+   def print_type_train(type)
+     puts "Список #{type} поездов:"
+     @trains[type].each{|train| puts train.num}
+   end
+
+   def print_all_train
+     puts "Список всех поездов на станции:"
+     #При получение массива значений(@trains.values)Почему то становится не доступен
+     #метод ".num". Не очень понимаю, почему.
+     # @trains.values.each{|i| puts i.num}
+     @trains.keys.each{ |type| @trains[type].each { |train| puts train.num}}
+   end
 end
 
 class Route
-  attr_accessor :route
+  attr_accessor :stations
 
   def initialize(start, ending)
-    @route =[start, ending]
+    @stations =[start, ending]
   end
 
   def add_station(station)
-    @route.insert(-2, station)
-    puts "В маршрут была добавлена станция #{station}"
+    @stations.insert(-2, station)
   end
 
   def delete_station(station)
-    if station == @route[0] || station == @route.last
-      puts "Это начальная/конечная станция. Нельзя удалять!"
-    else
-      @route.delete(station)
-      puts "Из маршрута удалена станция #{station}"
+    unless station == @stations[0] || station == @stations.last
+      @stations.delete(station)
     end
   end
   def all_station
-    puts @route
+    @stations.each{|station| puts station}
   end
 end
 
 class Train
-  attr_accessor :num
-  attr_accessor :type
-  attr_accessor :route
-  attr_accessor :current_station
+  attr_reader :num
+              :type
+              :route
+              :train_cars
+              :speed
+              :current_station
 
   def initialize(num, type, train_cars)
     @num = num
@@ -67,8 +72,8 @@ class Train
     @speed += speed
   end
 
-  def speed_now
-    puts "Скорость равна #{@speed}"
+  def decrease_speed(value)
+    @speed -= value if @speed - value >= 0
   end
 
   def stop
@@ -76,63 +81,44 @@ class Train
   end
 #вагоны
   def add_train_cars
-    if @speed == 0
-      @train_cars += 1
-    else
-      puts "Остановите поезд"
-    end
+      @train_cars += 1 if @speed == 0
   end
 
   def remove_train_cars
-    if @speed == 0
-      @train_cars -= 1
-    else
-      puts "Остановите поезд"
-    end
+      @train_cars -= 1 if @speed == 0 && @train_cars > 0
   end
 
   def all_train_cars
-    puts "Сейчас вагонов: #{@train_cars}"
+    return @train_cars
   end
   #маршрут
   def accept_route(route)
-    @route = route.route
-    self.current_station = self.route.first
-    puts "Поезд находится на станции #{current_station} и будет двигаться по марщруту #{self.route.first} - #{self.route.last}"
+    @route = route.stations
+    @current_station = 0
   end
 
-  def go_next_station
-    if self.current_station == self.route.last
-      puts "Это конечная станция, поезд дальше не идет"
-    else
-      self.current_station = self.route[self.route.index(self.current_station) + 1]
-      puts "Поезд прибыл на станцию #{self.current_station}"
-    end
+  def next_station
+    @current_station += 1 if @current_station < @route.length - 1
   end
 
-  def go_previous_station
-    if self.current_station == self.route.first
-      puts "Это первая станция, можно ехать только вперед"
-    else
-      self.current_station = self.route[self.route.index(self.current_station) - 1]
-      puts "Поезд приехал на станцию #{self.current_station}"
-    end
+  def previous_station
+    @current_station -= 1 if @current_station > 0
   end
 
   def show_next_station
-     if self.current_station == self.route.last
+     if @current_station == @route.length - 1
       puts "Это последняя станция"
     else
-      next_station = self.route[self.route.index(self.current_station) + 1]
+      next_station = @route[@current_station + 1]
       puts "Следущая станция: #{next_station}"
     end
   end
 
   def show_prev_station
-    if self.current_station == self.route.first
+    if @current_station == 0
       puts "Это первая станция"
     else
-      prev_station = self.route[self.route.index(self.current_station) - 1]
+      prev_station = @route[@current_station - 1]
       puts "Предыдущая станция: #{prev_station}"
     end
   end
